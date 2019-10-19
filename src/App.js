bb426b089a36cf10560290a33a2a9b004e65d0b3
Danielle Alba <danib.sgh@gmail.com>
2019-10-19 19:42:12 -0400

    import react component and made class app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
   return (
     <div className="App">
       <header className="App-header">
