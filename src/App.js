1b0d21c95a80fe2d7583cf0c8eb693cba1f048d8
Danielle Alba <danib.sgh@gmail.com>
2019-10-23 17:02:00 -0400

    fixed fetching

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ class App extends Component {
 
   fetchData = () => {
     fetch("https://dog.ceo/api/breeds/image/random")
-    .then(res => res.json)
-      .then(data => {
-        console.log(data)
-        this.setState({
-          doggo: data
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      this.setState({
+        doggo: data.message
         })
       })
 
